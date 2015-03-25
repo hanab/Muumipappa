a0d1b41e96a0c8f645f9f11b829201965ebd764d
Omid Hezaveh <a@gik.fi>
2015-03-25 11:50:14 +0200

    sorting sorted out

--- app/app.js
+++ app/app.js
@@ -3,7 +3,23 @@ Tasks = new Mongo.Collection("tasks");
 if (Meteor.isClient) {
 	Template.body.helpers({
 		tasks: function () {
-			return Tasks.find({});
+			return Tasks.find({}, {sort: {createdAt: -1}});
 		}
 	});
+	Template.body.events({
+		"submit .new-task": function (event) {
+
+			var text = event.target.text.value;
+
+			Tasks.insert({
+				text: text,
+				createdAt: new Date()
+			});
+			
+			// clear form
+			event.target.text.value = "";
+
+			return false;
+}
+});
 }
