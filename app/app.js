52c9c0e34bce504d64b387cc3ab87e3978756d19
Omid Hezaveh <a@gik.fi>
2015-03-25 11:17:23 +0200

    Mongo collections added

--- app/app.js
+++ app/app.js
@@ -1,9 +1,9 @@
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
 	Template.body.helpers({
-		tasks: [
-		{ text: "This is task 1"},
-		{ text: "this is task 2"},
-		{ text: "task 3"}
-		]
+		tasks: function () {
+			return Tasks.find({});
+		}
 	});
 }
